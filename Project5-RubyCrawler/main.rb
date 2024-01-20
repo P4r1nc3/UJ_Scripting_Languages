e1a7a0039398f79b9198119b5c1a158a1d0c54fc
parince <konrad.tupta@gmail.com>
2024-01-20 12:15:21 +0100

    Created an option to enable or disable saving products to the database

--- Project5-RubyCrawler/main.rb
+++ Project5-RubyCrawler/main.rb
@@ -3,17 +3,19 @@ require './crawler'
 require './product'
 require './database_manager'
 
-db_manager = DatabaseManager.new("products.db")
+additional_info = true
+database = true
+
+db_manager = DatabaseManager.new("products.db") if database
 
 puts("What are You looking for?: ")
 search = gets.chomp
 search.gsub!(" ", "+")
-additional_info = true
 
 crawler = Crawler.new(search, additional_info)
 items = crawler.get_main_items
 
 items.each do |item|
-    db_manager.save_product(item)
+    db_manager.save_product(item) if database
     puts item.to_string
 end
